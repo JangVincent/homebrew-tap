5f1732b9c737e5a37de2497a89edc6de39bb85a1
Phantola <phantoladev@gmail.com>
2023-11-27 14:33:27 +0900

    Update txview.rb v.0.1.2

--- Formula/txview.rb
+++ Formula/txview.rb
@@ -2,8 +2,8 @@ class Txview < Formula
   desc "CLI for view block-chain transaction"
   homepage "https://github.com/phantola/txview"
   url "https://github.com/Phantola/txview/releases/download/Latest/txview-0.1.1-apple-silicon.tar.gz"
-  sha256 "5f6be169240470e1cadc26cb7820e59092dc427983ca4708b6da2ba9c551acff"
-  version "0.1.1"
+  sha256 "7ca84abf61bda5b8fa473b6f887fc1cc6525cc6da705d5301c88296a04c3dbf8"
+  version "0.1.2"
 
   def install
     bin.install "txview"
