0c9bba28b2f07e93fbcbf75a3e8daec23c4be5de
JangVincent <vincentjangdev@gmail.com>
2026-05-06 11:28:04 +0900

    chore(cask): bump coagent-app to 0.1.24

--- Casks/coagent-app.rb
+++ Casks/coagent-app.rb
@@ -1,6 +1,6 @@
 cask "coagent-app" do
-  version "0.1.22"
-  sha256 "88a831351ea32769eb30f6d8a5e2fff9456190a14fd278838582607221231ac0"
+  version "0.1.24"
+  sha256 "1e31a93b142950e7f17b6d4441acd45cb5f33db3cc6a0342b3d9095cb1cd9c9f"
 
   url "https://github.com/JangVincent/coagent-app/releases/download/v#{version}/coagent-#{version}-arm64.dmg"
   name "Coagent"
